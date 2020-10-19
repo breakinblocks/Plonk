06f974d9891a4cd5cd4e7c9c300ebe0f23204d88
BlueAgent <BlueAgent@users.noreply.github.com>
2020-10-19 14:20:50 +0800

    Mark all methods and parameters as Nonnull by default

---
+++ src/main/java/com/breakinblocks/plonk/common/material/package-info.java
@@ -0,0 +1,7 @@
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+package com.breakinblocks.plonk.common.material;
+
+import mcp.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
