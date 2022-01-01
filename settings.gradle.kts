40f506ba0ac8773fdc4ffe6708b1138dfca71244
BlueAgent <BlueAgent@users.noreply.github.com>
2022-01-01 09:35:08 +0800

    Update ForgeGradle and Forge

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,9 +6,7 @@ pluginManagement {
                     useModule("net.kyori:blossom:1.3.0")
                 }
                 "net.minecraftforge.gradle" -> {
-                    // TODO: Change when merged and released: https://github.com/MinecraftForge/ForgeGradle/pull/763
-                    //useModule("net.minecraftforge.gradle:ForgeGradle:4.1.7")
-                    useModule("tk.sciwhiz12.gradle:ForgeGradle:4.1.9")
+                    useModule("net.minecraftforge.gradle:ForgeGradle:5.1.27")
                 }
             }
         }
@@ -21,13 +19,6 @@ pluginManagement {
                 includeGroup("net.minecraft")
                 includeGroup("net.minecraftforge")
                 includeGroup("net.minecraftforge.gradle")
-            }
-        }
-        maven {
-            name = "sciwhiz12"
-            url = uri("https://sciwhiz12.tk/maven")
-            content {
-                includeGroup("tk.sciwhiz12.gradle")
             }
         }
         gradlePluginPortal {
