02d24b312d190a4b6b1cdd2c3c33af973c320eb6
BlueAgent <BlueAgent@users.noreply.github.com>
2024-02-10 17:16:20 +0800

    Copy latest to v1.19.4 (restore)

---
+++ projects/minecraft/latest/forge/src/main/java/com/breakinblocks/plonk/common/tag/PlonkTags.java
@@ -0,0 +1,26 @@
+package com.breakinblocks.plonk.common.tag;
+
+import com.breakinblocks.plonk.Plonk;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+
+public class PlonkTags {
+    public static void init() {
+        Items.init();
+    }
+
+    public static class Items {
+
+        public static final TagKey<Item> UNPLACEABLE = tag("unplaceable");
+
+        public static void init() {
+        }
+
+        @SuppressWarnings("SameParameterValue")
+        private static TagKey<Item> tag(String name) {
+            return ItemTags.create(new ResourceLocation(Plonk.MOD_ID, name));
+        }
+    }
+}
